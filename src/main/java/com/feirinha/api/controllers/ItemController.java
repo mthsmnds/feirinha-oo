932bf10f8797ee731106cefd7d34647a62fd546d
Matheus Mendes <mathmendes.f@gmail.com>
2025-06-21 23:28:05 -0300

    tratamento de erros

--- src/main/java/com/feirinha/api/controllers/ItemController.java
+++ src/main/java/com/feirinha/api/controllers/ItemController.java
@@ -4,6 +4,9 @@ import java.lang.classfile.ClassFile.Option;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,43 +33,46 @@ public class ItemController {
     }
 
     @GetMapping()
-    public List<ItemModel> getItems(){
-        return itemRepository.findAll();
+    public ResponseEntity<Object> getItems(){
+        return ResponseEntity.status(HttpStatus.OK).body(itemRepository.findAll())
+        ;
     }
 
     @GetMapping("/{id}")
-    public Optional<ItemModel> getItemId(@PathVariable("id") Long id){
+    public ResponseEntity<Object> getItemId(@PathVariable("id") Long id){
         Optional<ItemModel> item = itemRepository.findById(id);
         
         if(!item.isPresent()){
-            return Optional.empty();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item com este ID não encontrado");
         }else{
-            return Optional.of(item.get());
+            return ResponseEntity.status(HttpStatus.OK).body(item.get());
         }
     }
 
     @PostMapping()
-    public void postItem(@RequestBody @Valid ItemDTO body){
+    public ResponseEntity<Object> postItem(@RequestBody @Valid ItemDTO body){
         ItemModel item = new ItemModel(body);
         itemRepository.save(item);
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
     }
 
     @PutMapping("/{id}")
-    public void updateItem(@PathVariable("id") Long id, @RequestBody @Valid ItemDTO body){
+    public ResponseEntity<Object> updateItem(@PathVariable("id") Long id, @RequestBody @Valid ItemDTO body){
         Optional<ItemModel> item = itemRepository.findById(id);
         
         if(!item.isPresent()){
-            //return Optional.empty();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
         ItemModel newItem = new ItemModel(body);
         newItem.setId(id);
         itemRepository.save(newItem);
-
+        return ResponseEntity.status(HttpStatus.OK).body(newItem);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteItem(@PathVariable("id") Long id){
+    public ResponseEntity<Object> deleteItem(@PathVariable("id") Long id){
         itemRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
